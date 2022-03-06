30ff79d19906006db69d16d140e36a54caac686a
fcmdhyd <fcmdhyd@gmail.com>
2022-03-06 22:49:29 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# with open("weather_data.csv",'r') as weather_file:
+#     data = weather_file.readlines()
+#     print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
